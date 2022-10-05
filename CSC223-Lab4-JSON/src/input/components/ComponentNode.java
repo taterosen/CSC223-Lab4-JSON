d0dc2d98b1670014b69c24f2d6fc56ccf755b87d
taterosen <taterosen@gmail.com>
2022-10-05 15:45:11 -0400

    Updated comments pt.2
    comments on ComponentNode

--- CSC223-Lab4-JSON/src/input/components/ComponentNode.java
+++ CSC223-Lab4-JSON/src/input/components/ComponentNode.java
@@ -2,8 +2,18 @@ package input.components;
 
 public interface ComponentNode
 {
+	/**
+	 * General unparse method to be overridden.
+	 * @param sb
+	 * @param level
+	 */
 	void unparse(StringBuilder sb, int level);
 	
+	/**
+	 * Indents the number of times specified by a given int.
+	 * @param level
+	 * @return
+	 */
 	default String indent(int level) {
 		String indents = "";
 		for(int i = 0; i < level; i++) 
